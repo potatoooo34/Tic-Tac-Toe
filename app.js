9025d74713f27e7761afc95eba6bf68a761194b4
potatoooo34 <108672710+potatoooo34@users.noreply.github.com>
2024-06-11 01:34:53 +0530

    Added the winner functionality

---
+++ app.js
@@ -0,0 +1,53 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = false;
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+boxes.forEach((box) =>{
+    box.addEventListener("click" , () =>{
+        console.log("box clicked");
+        if(turnO){
+            box.innerText = 'O';
+            turnO = false;
+        }
+        else{
+            box.innerText = 'X';
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    })
+})
+
+const showWinner = (winner) =>{
+    msg.innerText = `Congratulations , Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+const checkWinner = () =>{
+    for(let pattern of winPatterns){
+        let posval1 = boxes[pattern[0]].innerText;
+        let posval2 = boxes[pattern[1]].innerText;
+        let posval3 = boxes[pattern[2]].innerText;
+
+        if(posval1 != "" && posval2 != "" && posval3 != ""){
+            if(posval1 === posval2 && posval2 === posval3){
+                console.log("Winner");
+                showWinner(posval1);
+            }
+        }
+    }
+}
